3795f8b385bb26dacadeaf1f97b370998f7b64c0
OptimusJam <joseangel.martinezvera@gmail.com>
2015-08-04 22:15:41 +0200

    Sesiones y publicacion de comentarios

---
+++ controllers/user-controller.js
@@ -0,0 +1,18 @@
+var users = { admin: {id:1,   username:"admin",	password:"1234"},
+			  pepe:	 {id:2,	  username:"pepe",	password:"5678"},
+			  juan:	 {id:3,	  username:"juan",	password:"1471"}
+			};
+			
+//Comprueba si el usuario esta registrado en users
+//Si la autenticacion falla o hay errores, se ejecuta callback(error).
+exports.autenticar = function (login, password,callback) {
+	if (users[login]) {
+		if (password === users[login].password) {
+			callback(null, users[login]);
+		} else {
+			callback(new Error('Password incorrecto.'));
+		}	
+	} else {
+		callback(new Error('No existe el usuario.'));
+	}
+};
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz-controller');
 var commentController = require('../controllers/comment-controller');
+var sessionController = require('../controllers/session-controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -11,6 +12,8 @@ router.get('/', function(req, res) {
 
 //Autoload de comando con :quizId
 router.param('quizId',quizController.load);
+//Autoloas de comando para :commentId
+router.param('commentId',commentController.load);
 
 //router.get('/quizes/question',quizController.question);
 //router.get('/quizes/answer',quizController.answer);
@@ -19,18 +22,23 @@ router.get('/quizes',						quizController.index);
 router.get('/quizes/:quizId(\\d+)',			quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',	quizController.answer);
 
-router.get('/quizes/new',					quizController.new);
-router.post('/quizes/create',				quizController.create);
-
-router.get('/quizes/:quizId(\\d+)/edit',	quizController.edit);
-router.put('/quizes/:quizId(\\d+)',			quizController.update);
-
-router.delete('/quizes/:quizId(\\d+)',		quizController.destroy);
+router.get('/quizes/new',					sessionController.loginRequired, quizController.new);
+router.post('/quizes/create',				sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',	sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)',			sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)',		sessionController.loginRequired, quizController.destroy);
 
 router.get('/author', quizController.author);
+
+//Definicion de rutas de sesion
+router.get('/login',						sessionController.new);		//Formulario de login
+router.post('/login',						sessionController.create);	//Crear sesion
+router.get('/logout',						sessionController.destroy);	//Destruir sesion
 
 //Rutas de los comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',		commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+													sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
